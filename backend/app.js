22a6dcbc084b8670e11d1ce987eb7996d3d3e1be
vKnie <kevin.boillon@free.fr>
2025-05-06 15:34:31 +0200

    we

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const cors = require('cors');
 const app = express();
 
 // Configuration pour fonctionner correctement derrière un proxy HTTPS
